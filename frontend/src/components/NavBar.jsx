05719bf285287fcb93fb7ade26892a015f409155
RithishRM <pes2ug23cs458@pesu.pes.edu>
2025-11-08 09:59:32 +0530

    Fixed NavBar for Vercel

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,21 +1,15 @@
-// src/components/NavBar.jsx
-import { Link } from "react-router-dom";
+import React from 'react';
+// We removed 'Link' from here because it's no longer used
 
 export default function NavBar() {
   return (
     // Make Navbar sticky
     <nav className="bg-blue-700 text-white p-4 flex justify-between sticky top-0 z-10">
       <h1 className="text-xl font-bold">🌍 MediMind GHI</h1>
-      {/* Remove the old links, since they are in the sidebar now */}
-      {/* You can add user profile, settings, etc. here */}
+      {/* We removed the old links, since they are in the sidebar now */}
       <div>
         <span className="font-semibold">Global Health Dashboard</span>
       </div>
     </nav>
   );
 }
-
-// src/pages/Patients.jsx
-// I recommend renaming this file to `CountryList.jsx`
-// ... (add a search bar for extra polish!)
-// ...
